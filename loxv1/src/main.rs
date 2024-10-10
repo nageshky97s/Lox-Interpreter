885fee93a296c3cbef6ef81d09d3ca2c5c67ab39
incompleteness <nageshky97s@gmail.com>
2024-10-11 02:11:23 +0530

    Creation

--- loxv1/src/main.rs
+++ loxv1/src/main.rs
@@ -1,3 +1,13 @@
+use std::env;
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+
+    if args.len() > 1 {
+        println!("Usage: lox [script]");
+        std::process::exit(1);
+    } else if args.len() == 1 {
+        println!("File");
+    } else {
+        println!("Running");
+    }
 }
